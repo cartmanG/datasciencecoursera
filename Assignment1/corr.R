d228c2ce56154106d01fdcd93b215a348fafe64e
Carter Jee <carter.jee@hotmail.com>
2015-09-15 13:37:25 +1000

    finished assignment 1!!!!

--- Assignment1/corr.R
+++ Assignment1/corr.R
@@ -22,25 +22,20 @@ corr <- function(directory, threshold = 0) {
   #get loop length from nrows of ss_index_nobs
   loop_length<-nrow(ss_index_nobs)
   
-  #initiate data frame for binding
-  dat <- data.frame() 
-  
-  
   # --- Assert create an empty numeric vector
   corrsNum <- numeric(0)
   
   #loop through files and load in dat using rbind
+  if (loop_length==0){
+    corrsNum<=numeric(0)
+  } else{
+  
   for(i in 1:loop_length){
-    dat <- rbind(dat, read.csv(files_list[ss_index_nobs[i,1]]))
-  }
-  
-  
-  #calculate correlation of sulfate and nitrate for 
-  if (loop_length>0) {
+    dat <- read.csv(files_list[ss_index_nobs[i,1]])
     corrsNum <- c(corrsNum, cor(dat$sulfate, dat$nitrate, use = "pairwise.complete.obs"))
-    } else {
-    corrsNum <- numeric(0)
   }
+  }
+  
   
   return(corrsNum)
   
